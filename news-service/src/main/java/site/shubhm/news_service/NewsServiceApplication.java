0c0318d10e1ce60a043433d24043af1002d3dd21
shubh <shubhammane434@gmail.com>
2025-10-29 01:26:19 +0530

    add eureka-server configuration to project

--- news-service/src/main/java/site/shubhm/news_service/NewsServiceApplication.java
+++ news-service/src/main/java/site/shubhm/news_service/NewsServiceApplication.java
@@ -5,10 +5,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 @EnableScheduling
 @ComponentScan(basePackages = "site.shubhm.news_service")
 public class NewsServiceApplication {
--- twitter-service/src/main/java/site/shubhm/twitter_service/TwitterServiceApplication.java
+++ twitter-service/src/main/java/site/shubhm/twitter_service/TwitterServiceApplication.java
@@ -2,8 +2,11 @@ package site.shubhm.twitter_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
+
 public class TwitterServiceApplication {
 
 	public static void main(String[] args) {
