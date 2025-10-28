149a21e3c439b87711e3aa31243d43cd4099ed52
shubh <shubhammane434@gmail.com>
2025-10-28 16:03:59 +0530

    add Notification-service

---
+++ notification-service/src/test/java/site/shubhm/notification_service/NotificationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package site.shubhm.notification_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
